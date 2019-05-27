12a435c59a353d898a1bc5a42c682d7d44ec8980
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-05-27 21:34:52 +0100

    Extracting var declaration out of function

--- src/solutions/p1Multiplesof3And5/main.go
+++ src/solutions/p1Multiplesof3And5/main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
+var (
+	sum int
+)
+
 //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //Find the sum of all the multiples of 3 or 5 below 1000.
 func main() {
-	sum := 0
-
 	//We can start the loop at 3, seeing as this is the lowest factor.
 	for i := 3; i < 1000; i++ {
 		//If i is a multiple of 3 or 5, then add it to sum.
