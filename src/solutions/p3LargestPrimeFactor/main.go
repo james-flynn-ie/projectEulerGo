2f607f20c7355be9726c2ef008972ce2f8375660
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-05-31 19:10:40 +0100

    Solution

--- src/solutions/p3LargestPrimeFactor/main.go
+++ src/solutions/p3LargestPrimeFactor/main.go
@@ -2,23 +2,20 @@ package main
 
 import "fmt"
 
-//const maxnumber = 600851475143
+var primefactornumber int
 
 //The prime factors of 13195 are 5, 7, 13 and 29.
 //What is the largest prime factor of the number 600851475143 ?
 func main() {
-	numberUnderTest := 13195
+	numberUnderTest := 600851475143
 
 	//Keep dividing by the lowest prime number, which is 2, until mod 0 is reached.
 	for i := 2; i <= numberUnderTest; i++ {
-		// A prime factor must be >= 2 & mod = 0
-		if numberUnderTest%i == 0 {
-			fmt.Println(i)
-
+		if numberUnderTest%i == 0 && numberUnderTest != i {
 			// Divide by factor, then reset i to 2 and loop through again.
 			numberUnderTest = numberUnderTest / i
 			i = 2
 		}
 	}
-
+	fmt.Printf("\nThe largest prime factor of 600851475143 is: %d", numberUnderTest)
 }
