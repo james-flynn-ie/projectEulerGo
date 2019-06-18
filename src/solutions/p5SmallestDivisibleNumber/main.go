1e87051ecc90e703fc72eda339037fe069cf09b7
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-06-18 20:00:22 +0100

    Refactor: Improve Println, add comments

--- src/solutions/p5SmallestDivisibleNumber/main.go
+++ src/solutions/p5SmallestDivisibleNumber/main.go
@@ -2,21 +2,67 @@ package main
 
 import "fmt"
 
+var (
+	divisiblenumber         int
+	smallestdivisiblenumber int
+)
+
 /*
  * https://projecteuler.net/problem=5
  * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
  * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  */
 func main() {
-	i := 1
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 
-	// We increment i until it is no longer evenly divided by the numbers 1..20 (i.e.: until mod 1..20 != 0)
-	for i%2 != 0 || i%3 != 0 || i%4 != 0 || i%5 != 0 ||
-		i%6 != 0 || i%7 != 0 || i%8 != 0 || i%9 != 0 ||
-		i%10 != 0 || i%11 != 0 || i%12 != 0 || i%13 != 0 ||
-		i%14 != 0 || i%15 != 0 || i%16 != 0 || i%17 != 0 ||
-		i%18 != 0 || i%19 != 0 || i%20 != 0 {
-		i++
+	for _, num := range nums {
+		fmt.Println(lcm(20, num))
 	}
-	fmt.Printf("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is %v", i)
 }
+
+func gcd(a int, b int) int {
+	// Greatest Common Divisor https://en.wikipedia.org/wiki/Euclidean_algorithm
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a int, b int) int {
+	return a * b / gcd(a, b)
+}
+
+// Python:
+// def gcd(a, b):
+//     while b != 0:
+//         a, b = b, a % b
+//     return a
+
+// def lcm(a, b):
+//     return a*b/gcd(a, b)
+
+// print reduce(lcm, range(1, 20+1))
+
+
+// Java
+// public class Euler5 {
+//     final static int MAX_N = 20;
+
+//     static int gcd(int a, int b) {
+//         while (b != 0) {
+//             int t = a;
+//             a = b;
+//             b = t % b;
+//         }
+//         return a;
+//     }
+
+//     public static void main(String[] args) {
+//         int result = 1;
+//         for (int i = 1; i <= MAX_N; i++) {
+//             int currentGcd = gcd(i, result);
+//             result = result / currentGcd * i;
+//         }
+//         System.out.println(result);
+//     }
+// }
