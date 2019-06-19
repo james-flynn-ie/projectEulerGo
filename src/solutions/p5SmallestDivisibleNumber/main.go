47cca423cdffb8c4afc9c746bc3c896fd0ba1207
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-06-19 20:31:23 +0100

    Problem 5 Working solution for 1..10

--- src/solutions/p5SmallestDivisibleNumber/main.go
+++ src/solutions/p5SmallestDivisibleNumber/main.go
@@ -13,15 +13,18 @@ var (
  * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  */
 func main() {
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	result := 1
 
 	for _, num := range nums {
-		fmt.Println(lcm(20, num))
+		fmt.Println(lcm(result, num))
+		result = lcm(result, num)
 	}
+	fmt.Printf("Final result: %d", result)
 }
 
-func gcd(a int, b int) int {
-	// Greatest Common Divisor https://en.wikipedia.org/wiki/Euclidean_algorithm
+func gcf(a int, b int) int {
+	// Greatest Common Factor https://en.wikipedia.org/wiki/Euclidean_algorithm
 	for b != 0 {
 		a, b = b, a%b
 	}
@@ -29,5 +32,5 @@ func gcd(a int, b int) int {
 }
 
 func lcm(a int, b int) int {
-	return a * b / gcd(a, b)
+	return a * b / gcf(a, b)
 }
