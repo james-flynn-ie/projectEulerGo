3f9617ea7fdbf4cf7ee48b8264a7d4402f21d154
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-06-24 19:57:14 +0100

    p6 Refactor: Adding comments on calculations

--- src/solutions/p6SumSquareDifference/main.go
+++ src/solutions/p6SumSquareDifference/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 var (
+	difference   int
 	squaredsum   int
 	sumofnums    int
 	sumofsquares int
 )
 
-const maxnumber = 10
+const maxnumber = 100
 
 /*
  * https://projecteuler.net/problem=6
@@ -23,11 +24,24 @@ const maxnumber = 10
  */
 func main() {
 	for i := 1; i <= maxnumber; i++ {
-		sumofnums += i
+		/*
+		* Find the sum of all the squares from 1 to the highest natural number we
+		* are seeking in the sumofsquares var.
+		 */
 		sumofsquares += i * i
+
+		/*
+		* We also want to keep a sum of all the numbers (unsquared) for the next
+		* step, where we square the sum of all the numbers from 1 to the
+		* highest natural number we are seeking.
+		 */
+		sumofnums += i
 	}
 	fmt.Printf("The sum of the squares of the first %v natural numbers is: %v", maxnumber, sumofsquares)
 
 	squaredsum = sumofnums * sumofnums
-	fmt.Printf("\nThe squared sum of the first %v natural numbers is: %v", maxnumber, squaredsum)
+	fmt.Printf("\nThe squared sum of the squares of the first %v natural numbers is: %v", maxnumber, squaredsum)
+
+	difference = squaredsum - sumofsquares
+	fmt.Printf("\ndifference between the sum of the squares and the square of the sum for the first %v natural numbers is: %v", maxnumber, difference)
 }
