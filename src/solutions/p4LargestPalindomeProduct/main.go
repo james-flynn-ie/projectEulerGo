787f3f516e26447854aed401ea092296278154b5
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-06-26 21:56:24 +0100

    Refactor: Improve commenting

--- src/solutions/p4LargestPalindomeProduct/main.go
+++ src/solutions/p4LargestPalindomeProduct/main.go
@@ -13,13 +13,17 @@ var (
 /*
  * https://projecteuler.net/problem=4
  * A palindromic number reads the same both ways.
- * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * The largest palindrome made from the product of two 2-digit numbers is:
+ *    9009 = 91 × 99.
  * Find the largest palindrome made from the product of two 3-digit numbers.
  */
 func main() {
 	for i := 999; i > 0; i-- {
-		// j doesn't need to be initialized at 999, because setting it that high would duplicate the previous i values.
-		// i * j  is the same as j * i , e.g.: i (997) * j (996) would be the same as i (996) * j (997), so j = 996 would do.
+		/* j doesn't need to be initialized at 999, because setting it that
+		 * high would duplicate the previous i values.
+		 * i * j  is the same as j * i , e.g.: i (997) * j (996) would be the
+		 * same as i (996) * j (997), so j = 996 would do.
+		 */
 		for j := i; j > 0; j-- {
 			numberundertest := i * j
 
@@ -34,14 +38,20 @@ func main() {
 }
 
 func reverse(s string) string {
-	// UTF-8 String literals are a sequence of bytes, so we can switch them using this type.
+	/* UTF-8 String literals are a sequence of bytes, so we can switch them
+	 * using this type.
+	 */
 	bytes := []byte(s)
 
 	/*
-	 * Both counters, m and n, start at 0. We increment m upwards through the byte sequence.
-	 * We decrement n so that it counts from the last [-1] position in the String towards the start.
+	 * Both counters, m and n, start at 0. We increment m upwards through the
+	 * byte sequence.
+	 *
+	 * We decrement n so that it counts from the last [-1] position in the
+	 * String towards the start.
 	 * We then swap the byte found at m with the byte at n, and vice versa.
-	 * Once the counter m becomes equal to n, then we are finished swapping all of the bytes.
+	 * Once the counter m becomes equal to n, then we are finished swapping
+	 * all of the bytes.
 	 */
 	for m, n := 0, len(bytes)-1; m < n; m, n = m+1, n-1 {
 		bytes[m], bytes[n] = bytes[n], bytes[m]
