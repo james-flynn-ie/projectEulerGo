cafe4ef6b5cf38d9b4b6d05ccaa90c8e3efbfde3
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-07-13 10:09:40 +0100

    Adding problem description, const for max number

--- src/solutions/p7TenThousandAndFirstPrime/main.go
+++ src/solutions/p7TenThousandAndFirstPrime/main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+var highestprimenumber int
+
+const maxcountervalue = 10001
+
+/*
+ * https://projecteuler.net/problem=7
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ * What is the 10 001st prime number?
+ */
 func main() {
-
-	fmt.Println("Problem 7 is a work in progress, Please check back soon!")
-
+	fmt.Printf("\nThe 10,001st prime factor is: %d", highestprimenumber)
 }
