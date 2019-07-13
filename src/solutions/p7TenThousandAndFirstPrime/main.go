238a3efd1b80e7e947154bc034fb70b65dc3a4e7
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-07-13 10:45:10 +0100

    Reproduced Q solution, using channels to check primes

--- src/solutions/p7TenThousandAndFirstPrime/main.go
+++ src/solutions/p7TenThousandAndFirstPrime/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var highestprimenumber int
 
-const maxcountervalue = 10001
+const maxcountervalue = 6
 
 /*
  * https://projecteuler.net/problem=7
@@ -12,5 +12,33 @@ const maxcountervalue = 10001
  * What is the 10 001st prime number?
  */
 func main() {
-	fmt.Printf("\nThe 10,001st prime factor is: %d", highestprimenumber)
+	// For a description on how to use channels, see: https://tour.golang.org/concurrency/2
+	ch := make(chan int)
+	go generatenumbersequence(ch)
+
+	// Start at 2, as 1 is known to be a prime.
+	for i := 2; i <= maxcountervalue; i++ {
+		prime := <-ch
+		ch1 := make(chan int)
+		go filterprimenumbers(ch, ch1, prime)
+		ch = ch1
+	}
+
+	highestprimenumber := <-ch
+	fmt.Printf("\nThe %dth prime factor is: %d", maxcountervalue, highestprimenumber)
+}
+
+func filterprimenumbers(in <-chan int, out chan<- int, prime int) {
+	for {
+		i := <-in
+		if i%prime != 0 {
+			out <- i
+		}
+	}
+}
+
+func generatenumbersequence(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
 }
